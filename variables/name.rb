d7293f93fbf9c124a53d033dcc5db6a6e0c957bf
Louis Mascari <Lmascari16@gmail.com>
2022-05-18 00:00:09 +0000

    adding Variables exercises

---
+++ variables/name.rb
@@ -0,0 +1,14 @@
+#puts "Hey, what's your name?"
+#name = gets.chomp
+#puts "Nice to meet you, #{name}!"
+
+#10.times do
+#    puts name
+#end
+
+puts "What's your first name?"
+first_name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts "Your full name is " + first_name + " " + last_name
